8ba9b81ea2b3e0b681a812bb57129d42accd19e7
Ho Sy Quan <hoquan06012004@gmail.com>
2025-07-08 00:36:27 +0700

    [01] init

---
+++ Example01/DB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Example01
+{
+	internal class DB
+	{
+		public static SqlConnection Connect() {
+			SqlConnection conn = null;
+            //conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=cafe;User ID=sa;Password=1234$;MultipleActiveResultSets=true;");
+            conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=cafe;Integrated Security=True;MultipleActiveResultSets=true;");
+
+            return conn;
+		}
+	}
+}
